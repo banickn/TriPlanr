7ae75f5d2815bea781d08e78255ad779fa5340e7
Norman Banick <norman.banick@br-automation.com>
2016-11-23 22:09:54 +0100

    Remove double migration

--- db/migrate/20161123144918_create_periodization_years.rb
+++
@@ -1,9 +0,0 @@
-class CreatePeriodizationYear < ActiveRecord::Migration[5.0]
-  def change
-    create_table :periodization_years do |t|
-      t.string :title
-
-      t.timestamps
-    end
-  end
-end
