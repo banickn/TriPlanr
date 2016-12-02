fda72291a9bbe53ac7612791fec737e1596b4cc8
Norman Banick <norman.banick@br-automation.com>
2016-12-02 20:08:53 +0100

    Remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
 
   match 'tsscalc' => 'pages#index', via: :all
   match 'pmc' => 'pages#pmc', via: :all
-  match 'atp' => 'pages#atp', via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
